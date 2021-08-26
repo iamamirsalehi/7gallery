d2cc6c910ced8d43341f8f51a99d5301f39be428
Amir Salehi <iamamirsalehi@gmail.com>
2021-08-26 10:14:23 +0430

    users table completed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->char('mobile', 11)->nullable();
+            $table->enum('role', ['admin', 'user']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
