ceae3a19c93457aa3d6cd0fe9a19c1099d866d8f
Amir Salehi <iamamirsalehi@gmail.com>
2021-08-26 13:12:53 +0430

    admin base balde implemented

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('products/all', function (){
     return view('frontend.products.all');
 });
+
+Route::get('admin/index', function () {
+    return view('admin.index');
+});
+
+Route::get('admin/users', function () {
+    return view('admin.users.index');
+});
