b01a55f41d8d04f721aa343582c6341cc8d5eba6
Amir Salehi <iamamirsalehi@gmail.com>
2021-08-26 12:16:45 +0430

    frontend balde implemented

---
+++ resources/views/layouts/frontend/master.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.frontend.header')
+    
+   @yield('content')
+
+@extends('layouts.frontend.footer')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('create/category', function (){
+   Category::find(1)->update([
+       'title' => '7learn',
+   ]);
 });
